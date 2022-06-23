315e314b29714f60d7977f63c0bacfc4c236400f
hanbl <hanblq@yonyou.com>
2022-06-23 23:21:04 +0800

    feat(feat): 项目创建
    项目创建

---
+++ src/main/java/com/example/springdragoncommon/SpringDragonCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdragoncommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDragonCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDragonCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdragoncommon/SpringDragonCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdragoncommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDragonCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
