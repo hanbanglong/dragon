d7c67aa1144611ebce7537d26faf7730f4d832b5
hanbl <hanblq@yonyou.com>
2022-06-25 14:39:01 +0800

    feat(feat): 添加时间转换器以及pom中版本修改
    添加时间转换器以及pom中版本修改
    
    Closes #1

---
+++ src/main/java/com/example/springdragoncommon/hbl/yms/web/controller/ConversionController.java
@@ -0,0 +1,30 @@
+package com.example.springdragoncommon.hbl.yms.web.controller;
+
+import com.example.springdragoncommon.hbl.yms.web.bean.vo.ConversionVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author hanbl
+ */
+@RestController
+@RequestMapping("/yms/conversion/")
+public class ConversionController {
+    private static Logger logger = LoggerFactory.getLogger(ConversionController.class);
+
+    @RequestMapping("saveConversion")
+    public String saveConversion(ConversionVo conversionVo){
+        logger.info(String.format("获取到的请求参数为：[%s]",conversionVo));
+        return "保存成功！";
+    }
+
+    @RequestMapping("saveConversionByJson")
+    public String saveConversionByJson(@RequestBody ConversionVo conversionVo){
+        logger.info(String.format("获取到的请求参数为：[%s]",conversionVo));
+        return "保存成功！";
+    }
+
+}
